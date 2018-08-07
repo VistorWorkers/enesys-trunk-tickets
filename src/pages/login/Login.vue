42fc077a921ade5b95d15c99aeeb776f0f67f1ed
VistorWorkers <709085116@qq.com>
2018-08-07 09:18:20 +0800

    登录页界面完善

--- src/pages/login/Login.vue
+++ src/pages/login/Login.vue
@@ -1,21 +1,28 @@
 <template>
     <div class="login-page">
         <div class="container">
-            <div class="col-xs-12 logo">
+            <div class="logo">
                 <img src="@assets/images/login/login-logo.png"/>
             </div>
-            <form class="col-xs-12 p-0">
-
-            </form>
-            <div>
-                账号
-                <input type="text" name="loginAccount" v-model="loginAccount">
+            <div class="loginPanel">
+                <div class="input-group">
+                    <span class="input-group-addon">
+                        <i class="glyphicon glyphicon-user"></i>
+                    </span>
+                    <input type="text" v-model="request.loginAccount" placeholder="账号">
+                </div>
+                <div class="input-group m-t-20">
+                    <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+                    <input type="password" v-model="request.password" placeholder="密码">
+                </div>
+                <div class="login-buttons m-t-30">
+                    <button type="button" class="col-md-12 btn btn-block login-button"
+                            :class="loginBtn.class"
+                            @click="handleLoginClick">
+                        {{loginBtn.text}}
+                    </button>
+                </div>
             </div>
-            <div>
-                密码
-                <input type="password" name="password" v-model="password">
-            </div>
-            <button @click="handleLoginClick">登录</button>
         </div>
     </div>
 </template>
@@ -23,19 +30,33 @@
 <script>
     import store from '@store'
     import {login} from '@service'
+    import {SET_ISLOGIN} from '@store/mutation-types.js'
+
+    const BTN_LOGIN_WAIT = {
+        text : '立即登入',
+        class: ''
+    };
+    const BTN_LOGIN_NO_INPUT = {};
+    const BTN_LOGIN_LOGNING = {};
+    const BTN_LOGIN_SUCCESS = {};
+    const BTN_LOGIN_OFF_LINE = {};
+    const BTN_LOGIN_ERROR = {};
 
     export default {
         name   : 'Login',
         data () {
             return {
-                loginAccount  : '', // 账号
-                password      : '', // 密码
-                registrationID: '', // 极光推送id
-                longitude     : '', // 经度
-                latitude      : '', // 纬度
-                serialNo      : '', // 设备序列号
-                deviceModel   : '', // 设备型号
-                deviceUUID    : ''  // 设备唯一标识
+                request : {
+                    loginAccount  : '', // 账号
+                    password      : '', // 密码
+                    registrationID: '', // 极光推送id
+                    longitude     : '', // 经度
+                    latitude      : '', // 纬度
+                    serialNo      : '', // 设备序列号
+                    deviceModel   : '', // 设备型号
+                    deviceUUID    : ''  // 设备唯一标识
+                },
+                loginBtn: BTN_LOGIN_WAIT
             }
         },
         methods: {
@@ -47,12 +68,10 @@
                     loginAccount,
                     password
                 }).then(function ({data}) {
-                    store.commit('SET_ISLOGIN', {staffName: "张三", staffId: "2018273625241"})
+                    store.commit(SET_ISLOGIN, {staffName: "张三", staffId: "2018273625241"})
                     this.$router.push('/index')
                 }).catch(function (res) {
-
                 });
-
             }
         }
     }
@@ -61,23 +80,60 @@
 <style scoped lang="stylus" rel="stylesheet/stylus">
     @import "~@assets/styles/mixins.styl"
     .login-page
-        background: url("~@assets/images/login/login.png") 0 0 no-repeat;
-        background-size: cover;
-        background-position: bottom;
+        background url("~@assets/images/login/login.png") 0 0 no-repeat
+        background-size cover
+        background-position bottom
         fullPage()
         .container
-            position: absolute;
-            top: 0;
-            bottom: 0;
-            left: 0;
-            right: 0;
-            margin: auto;
-            max-width 375px;
-            height 250px;
+            position absolute
+            top 0
+            bottom 0
+            left 0
+            right 0
+            margin auto
+            max-width 375px
+            height 250px
+            padding 0 40px
             .logo
-                transition transform .3s .5s;
-                transform: translateY(0);
+                transition transform .3s .5s
+                transform translateY(0)
                 img
-                    height: auto;
-                    width: 100%;
+                    height auto
+                    width 100%
+            .loginPanel
+                margin-top 30%
+                .input-group
+                    border #73736e 1px solid
+                    background #73736e
+                    box-shadow rgba(0, 0, 0, .5) 0 2px 7px inset
+                    border-radius 5px
+                    height 40px
+                    margin-top 20px
+                    &:first-child
+                        margin 0
+                    .input-group-addon
+                        background transparent
+                        border none
+                        color #ffffff
+                    input
+                        width 100%
+                        height 100%
+                        padding-right 12px
+                        background transparent
+                        border none
+                        color #ffffff
+                .login-button
+                    background linear-gradient(to bottom, rgba(253, 139, 0, 1) 0%, rgba(255, 85, 0, 1) 100%)
+                    color #fff
+                    font-size 16px
+                    padding 7px
+                    border none
+                    border-radius 5px
+                    margin-top 30px
+                    &.login-success
+                        background linear-gradient(to bottom, rgba(54, 148, 0, 1) 0%, rgba(47, 117, 0, 1) 100%)
+                    &.login-danger
+                        background linear-gradient(to bottom, rgba(166, 118, 23, 1) 0%, rgba(153, 81, 5, 1) 100%)
+                    &.login-warning
+                        background linear-gradient(to bottom, rgba(189, 66, 55, 1) 0%, rgba(158, 46, 33, 1) 100%)
 </style>
